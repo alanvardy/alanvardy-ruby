af410c9ca173597a15bc40ec9a30526909e067a6
Alan Vardy <alanvardy@gmail.com>
2018-08-27 21:03:14 -0600

    Basic css and views set up

--- app.rb
+++ app.rb
@@ -2,9 +2,23 @@ require "sinatra"
 
 class App < Sinatra::Base
   get "/" do
+    @title = "Home"
     erb :index
   end
   get "/blog" do
+    @title = "Blog"
     erb :blog
+  end
+  get "/portfolio" do
+    @title = "Portfolio"
+    erb :portfolio
+  end
+  get "/resume" do
+    @title = "Resume"
+    erb :resume
+  end
+  get "/contact" do
+    @title = "Contact"
+    erb :contact
   end
 end
