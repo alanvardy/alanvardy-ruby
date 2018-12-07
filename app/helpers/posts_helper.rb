4449c50a75eb2b5182f700e4bc1a4461fb89e575
Alan Vardy <alanvardy@gmail.com>
2018-12-07 10:12:25 -0700

    finish external links

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,18 +1,8 @@
 # frozen_string_literal: true
+include ApplicationHelper
 
 module PostsHelper
-  require 'redcarpet'
-  require 'rouge'
-  require 'rouge/plugins/redcarpet'
-  # Custom renderer for Markdown syntax
-  class BlogRender < Redcarpet::Render::HTML
-    def initialize(extensions = {})
-      super extensions.merge(link_attributes: { target: '_blank' })
-    end
-    include Rouge::Plugins::Redcarpet
-  end
-
   def markdown_text(text)
-    Redcarpet::Markdown.new(BlogRender, fenced_code_blocks: true).render(text).html_safe
+    Redcarpet::Markdown.new(MarkdownRender, fenced_code_blocks: true).render(text).html_safe
   end
 end
--- app/helpers/setup_helper.rb
+++ app/helpers/setup_helper.rb
@@ -1,18 +1,9 @@
+include ApplicationHelper
+
 module SetupHelper
-  require 'redcarpet'
-  require 'rouge'
-  require 'rouge/plugins/redcarpet'
-  # Custom renderer for Markdown syntax
-  class BlogRender < Redcarpet::Render::HTML
-    def initialize(extensions = {})
-      super extensions.merge(link_attributes: { target: '_blank' })
-    end
-    include Rouge::Plugins::Redcarpet
-  end
-  
   def markdown_file(filename)
     path = Rails.root.join('app', 'assets', 'markdowns', filename)
     text = File.read(path)
-    Redcarpet::Markdown.new(BlogRender, fenced_code_blocks: true).render(text).html_safe
+    Redcarpet::Markdown.new(MarkdownRender, fenced_code_blocks: true).render(text).html_safe
   end
 end
