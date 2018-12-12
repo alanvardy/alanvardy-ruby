40d288a3d5424b043f1917189d7d9a272397c81e
Alan Vardy <alanvardy@gmail.com>
2018-12-12 12:04:09 -0700

    abstracted message send method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,7 @@ class MessagesController < ApplicationController
 def create
     @message = Message.new message_params
 if @message.valid?
-      mg_client = Mailgun::Client.new ENV['mailgun_secret_api_key']
-      email_params = {
-        from: message_params[:email],
-        to:   'alan@alanvardy.com',
-        subject: "Email from #{message_params[:name]} - alanvardy.com",
-        text:    "From: #{message_params[:name]}, Email: #{message_params[:email]}, Phone: #{message_params[:phone_number]} \n\n #{message_params[:body]}"
-                    }
-      mg_client.send_message 'mg.alanvardy.com', email_params
+      @message.send
       redirect_to root_path
       flash[:success] = "I have received your message and will be in touch soon!"
     else
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,15 @@ class Message
   include ActiveModel::Model
   attr_accessor :name, :email, :phone_number, :body
   validates :name, :email, :phone_number, :body, presence: true
+
+  def send
+    mg_client = Mailgun::Client.new ENV['mailgun_secret_api_key']
+    info = {
+      from: email,
+      to:   'alan@alanvardy.com',
+      subject: "Email from #{name} - alanvardy.com",
+      text:    "From: #{name}, Email: #{email}, Phone: #{phone_number} \n\n #{body}"
+                  }
+    mg_client.send_message 'mg.alanvardy.com', info
+  end
 end
