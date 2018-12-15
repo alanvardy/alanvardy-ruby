a06ccab2745b120875cd8c72502f51ce89111356
Alan Vardy <alanvardy@gmail.com>
2018-12-15 14:22:15 -0700

    local postgres setup done

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,19 @@
 
 ActiveRecord::Schema.define(version: 2018_12_14_220623) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "taggings", force: :cascade do |t|
-    t.integer "tag_id"
-    t.integer "post_id"
+    t.bigint "tag_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_taggings_on_post_id"
@@ -37,12 +37,6 @@ ActiveRecord::Schema.define(version: 2018_12_14_220623) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-  end
-
+  add_foreign_key "taggings", "posts"
+  add_foreign_key "taggings", "tags"
 end
