a3a519080ded9c6cd9f71193afedab5c27d0ee32
Alan Vardy <alanvardy@gmail.com>
2019-03-28 20:55:45 -0600

    formatting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,25 @@
+# frozen_string_literal: true
+
 class MessagesController < ApplicationController
   def new
     @message = Message.new
   end
-def create
+
+  def create
     @message = Message.new message_params
-if @message.valid?
+    if @message.valid?
       @message.send
       redirect_to root_path
-      flash[:success] = "I have received your message and will be in touch soon!"
+      flash[:success] = 'I have received your message and will be in touch soon!'
     else
-      flash[:warning] = "There was an error sending your message. Please try again."
+      flash[:warning] = 'There was an error sending your message. Please try again.'
       render :new
     end
   end
-private
-def message_params
+
+  private
+
+  def message_params
     params.require(:message).permit(:name, :email, :phone_number, :body)
-  end
+    end
 end
