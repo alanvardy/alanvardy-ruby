1e31636b8f72a81d6a6842195afc081f2a9f9805
Alan Vardy <alanvardy@gmail.com>
2019-08-18 16:29:51 -0600

    update to rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# typed: false
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,44 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_221_224_259) do
+ActiveRecord::Schema.define(version: 2018_12_21_224259) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'title'
-    t.text 'content'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'projects', force: :cascade do |t|
-    t.string 'title'
-    t.string 'description'
-    t.string 'weburl'
-    t.string 'giturl'
-    t.string 'image'
-    t.text 'more'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'featured'
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "weburl"
+    t.string "giturl"
+    t.string "image"
+    t.text "more"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "featured"
   end
 
-  create_table 'taggings', force: :cascade do |t|
-    t.bigint 'tag_id'
-    t.bigint 'post_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_taggings_on_post_id'
-    t.index ['tag_id'], name: 'index_taggings_on_tag_id'
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_taggings_on_post_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
-  create_table 'tags', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'taggings', 'posts'
-  add_foreign_key 'taggings', 'tags'
+  add_foreign_key "taggings", "posts"
+  add_foreign_key "taggings", "tags"
 end
